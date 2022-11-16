62231acd7b2d87079ecb1076b76f38074fbecc6e
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-16 18:35:15 +0530

    Adding simple linked list

--- src/com/bridgelabz/customlinkedlist/SimpleLinkedList.java
+++ src/com/bridgelabz/customlinkedlist/SimpleLinkedList.java
@@ -1,7 +1,56 @@
 package com.bridgelabz.customlinkedlist;
 
+import com.bridgelabz.customlinkedlist.SimpleLinkedList;
+import com.bridgelabz.customlinkedlist.SimpleLinkedList.Node;
+
 public class SimpleLinkedList {
-public static void main(String[] args) {
-	System.out.println("Welcome to Custom Linked list");
-}
+	Node head;
+
+	static class Node {
+		int data;
+		Node next;
+
+		public Node(int data2) {
+			this.data = data2;
+			this.next = null;
+		}
+	}
+
+	public void append(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = new Node(data);
+			return;
+		}
+		newNode.next=null;
+		Node lastNode = head;// 10
+		while (lastNode.next != null) {// lastNode.next=10
+			lastNode = lastNode.next; // lastNode=10;
+		}
+		lastNode.next = newNode;
+		return;
+	}
+
+	
+	public void printList() {
+		if (head == null) {
+			System.out.println("Linked list is empty!");
+			return;
+		}
+		Node tNode = head;
+		while (tNode != null) {
+			System.out.print(tNode.data + "==>");
+			tNode = tNode.next;
+		}
+		System.out.println("null");
+	}
+ 	public static void main(String[] args) {
+		SimpleLinkedList ll = new SimpleLinkedList();
+		ll.append(56);
+		ll.append(30);
+		ll.append(70);
+		ll.printList();
+
+	}
+
 }
